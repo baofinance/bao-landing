55b203f4806d878e7b23fe6e33127320b28096cf
Haruxe <haruxe@proton.me>
2024-07-22 13:36:57 -0700

    fix build errors2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,9 @@
 import Head from 'next/head'
-
 import { CallToAction } from '@/components/CallToAction'
-import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import { ProtocolData } from '@/components/ProtocolData'
-import { TertiaryFeatures } from '@/components/TertiaryFeatures'
 import { ActiveVaults } from '@/components/ActiveVaults'
 
 export default function Home() {
